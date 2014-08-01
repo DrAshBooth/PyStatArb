cfe92716eb40d4404ed30c5be870b1bcf20bd0de
ab24v07 <ash.booth@soton.ac.uk>
2014-08-01 21:56:37 +0100

    first commit

---
+++ PyStatArb/src/generating_timeseries.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def gaussian_white_noise(n, mu, sigma):
+	return np.random.normal(mu, sigma, n)
+
+def moving_average(n, mu, sigma, beta):
+	wn = gaussian_white_noise(n+1, mu, sigma)
+	ma = wn[1:] + beta*wn[:-1]
+	return ma
+
+if __name__ == "__main__":
+	import matplotlib.pyplot as plt
+	import pandas as pd
+	
+	ts = pd.Series(autoregressive_process(100,0,0.5,0.8,1), index=pd.date_range('1/1/2000', periods=100))
+	
+	ts.plot()
+	
+	plt.show()
+	
